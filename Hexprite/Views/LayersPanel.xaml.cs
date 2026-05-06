7df6f3f9897a185c20f7664ccf65b1ec60cc83ec
Intisar Bin Naim <showmik.haque@gmail.com>
2026-05-06 15:45:50 +0600

    feat: add basic struction of layer

---
+++ Hexprite/Views/LayersPanel.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Hexprite.Views
+{
+    public partial class LayersPanel : UserControl
+    {
+        public LayersPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
